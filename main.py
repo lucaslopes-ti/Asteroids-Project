da80b107f06ed8b6da1ab35d2f1f73d0bdbabaa3
Lucas Lopes <lucas.dalps@gmail.com>
2026-05-18 23:34:13 -0300

    Implementando funcoes no jogador

--- main.py
+++ main.py
@@ -1,26 +1,34 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from constants import *
 from logger import log_state
+from player import Player
+
 
 def main():
-	print("Starting Asteroids with pygame version: 2.6.1")
-	print(f"Screen width: {SCREEN_WIDTH}")
-	print(f"Screen height: {SCREEN_HEIGHT}")
-	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    print("Starting Asteroids with pygame version: 2.6.1")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
-	clock = pygame.time.Clock()
-	dt = 0
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
-	
-	while True:
-		log_state()
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				return
-		screen.fill("black")
-		pygame.display.flip()
-		dt = clock.tick(60) / 1000
-		
+    clock = pygame.time.Clock()
+    dt = 0
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        dt = clock.tick(60) / 1000
+       
+        player.update(dt)
+
+        log_state()
+
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
