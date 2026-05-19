e85c94677383b08e00db13f2207f0b3f50add5ef
Lucas Lopes <lucas.dalps@gmail.com>
2026-05-18 23:34:12 -0300

    feat: implement CircleShape base class and Player entity with rotation logic

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ def main():
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+	clock = pygame.time.Clock()
+	dt = 0
+
 	
 	while True:
 		log_state()
@@ -15,6 +19,7 @@ def main():
 				return
 		screen.fill("black")
 		pygame.display.flip()
+		dt = clock.tick(60) / 1000
 		
 
 
